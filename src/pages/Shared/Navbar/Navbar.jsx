21b194b16d5cefa4c61a6b0ececf7072807f9dfd
Remon Tripura <remontripura045@gmail.com>
2023-06-06 22:09:12 +0600

    navbar complete

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -4,14 +4,21 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     const navItem = <>
         <li><Link to="/">Home</Link></li>
+        <li><Link to="/instructors">Instructors</Link></li>
+        <li><Link to="/class">Classes</Link></li>
     </>
     return (
-        <div className="navbar max-w-7xl mx-auto">
+        <div className="navbar max-w-7xl mx-auto font-semibold uppercase bg-[#373737] text-white">
             <div className="navbar-start">
                 <Link to="/"><img className="w-10" src="https://i.ibb.co/3N7M5Ks/logo.png" alt="" /></Link>
             </div>
+            <div className="navbar-center lg:flex ">
+                <ul className="menu menu-horizontal lg:flex hidden px-1">
+                    {navItem}
+                </ul>
+            </div>
             <div className="navbar-end lg:flex ">
-            <div className="dropdown dropdown-end">
+                <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
@@ -19,9 +26,7 @@ const Navbar = () => {
                         {navItem}
                     </ul>
                 </div>
-                <ul className="menu menu-horizontal lg:flex hidden px-1">
-                    {navItem}
-                </ul>
+                <button className="mx-5 px-5 py-2 bg-white text-black">Login</button>
             </div>
 
         </div>
