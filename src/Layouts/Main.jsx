7f206a8fbca5d2c428cd7354f7f4db6286d67c0f
Remon Tripura <remontripura045@gmail.com>
2023-06-07 08:08:18 +0600

    login form setup

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -5,10 +5,10 @@ import Navbar from "../pages/Shared/Navbar/Navbar";
 const Main = () => {
     const location = useLocation();
     console.log(location)
-    const noHeaderFooter = location.
+    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('register')
     return (
         <div>
-            <Navbar></Navbar>
+            {noHeaderFooter || <Navbar></Navbar>}
             <Outlet></Outlet>
         </div>
     );
