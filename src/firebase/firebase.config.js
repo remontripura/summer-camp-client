a8374db45e630097cb788d6ee119b41b6573e27b
Remon Tripura <remontripura045@gmail.com>
2023-06-07 07:20:10 +0600

    login form creating

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqXiIlYLhtB7aeJ3m_Hc7DQ_xmYohVods",
+  authDomain: "wolves-1bcf8.firebaseapp.com",
+  projectId: "wolves-1bcf8",
+  storageBucket: "wolves-1bcf8.appspot.com",
+  messagingSenderId: "348520118274",
+  appId: "1:348520118274:web:e89e27a5910e70856ccf1b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
