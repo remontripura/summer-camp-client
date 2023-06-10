e69e803ac2f8090df4da5ed30aac5fc1ca55b9ae
Remon Tripura <remontripura045@gmail.com>
2023-06-10 16:31:57 +0600

    use axios in hooks

--- src/hooks/UseCard.jsx
+++ src/hooks/UseCard.jsx
@@ -1,21 +1,20 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
+import useAxiosSecure from './useAxiosSecure';
+
 
 
 const useCard = () => {
     const { user } = useContext(AuthContext);
     const token = localStorage.getItem('access-token')
+    const [axiosSecure] = useAxiosSecure();
 
     const { refetch, data: data = [] } = useQuery({
         queryKey: ['select', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/select?email=${user?.email}`, {
-                headers: {
-                    authorization: `bearer ${token}`
-                }
-            })
-            return response.json();
+            const response = await axiosSecure(`/select?email=${user?.email}`)
+            return response.data;
         },
 
     })
